35256c551d8748511b8c17143a5e8c2059e83c4a
t-downing <downing.tristan@gmail.com>
2024-05-20 14:27:28 -0700

    rm locale

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-import locale
-
 import dash_bootstrap_components as dbc
 from dash import Dash
 
 from callbacks.callbacks import register_callbacks
 from data.load_data import load_data
 from index import layout
-
-locale.setlocale(locale.LC_TIME, "fr_FR.UTF-8")
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = "Cadre action anticipatoire ouragans Haïti : déclencheurs historiques"
