24a55ccfe1c6de69dbf6a3a00fc5df383fd3bbd1
t-downing <downing.tristan@gmail.com>
2024-06-11 13:11:24 -0700

    undo debug

--- src/datasources/nhc.py
+++ src/datasources/nhc.py
@@ -17,5 +17,4 @@ def load_hist_fcast_monitors():
 
 def load_hti_distances():
     blob_name = f"{blob.PROJECT_PREFIX}/processed/noaa/nhc/historical_forecasts/hti_distances_2000_2023.parquet"
-    print(blob_name)
     return pd.read_parquet(BytesIO(blob.load_blob_data(blob_name)))
